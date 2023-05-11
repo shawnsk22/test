4170d5ba8fa9c73ef5b06e3f641a4615771db056
michellebrc <>
2023-05-11 17:09:15 +0800

    createnewproject

---
+++ js/game.js
@@ -0,0 +1,30 @@
+// Generate a random number between 1 and 100
+const number = Math.floor(Math.random() * 100) + 1;
+
+// Get the input element and submit button from the HTML
+const input = document.getElementById("guess");
+const submit = document.getElementById("submit");
+
+// Add click event listener to the submit button
+submit.addEventListener("click", function() {
+	// Get the value of the input element
+	const guess = parseInt(input.value);
+
+	// Check if the guess is correct, too low, or too high
+	if (guess === number) {
+		displayResult("Congratulations! You guessed the number!");
+	} else if (guess < number) {
+		displayResult("Your guess is too low. Try again.");
+	} else {
+		displayResult("Your guess is too high. Try again.");
+	}
+});
+
+// Function to display result to the user
+function displayResult(message) {
+	// Get the display element from the HTML
+	const display = document.getElementById("display");
+
+	// Set the message as the text content of the display element
+	display.textContent = message;
+}
